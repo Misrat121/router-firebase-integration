02817c583e5382f2678b0978252f44990229848c
Misrat121 <you@example.com>
2022-04-09 22:55:23 +0600

    hooks added

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import app from "../firebase.init";
-import { getAuth,GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth,GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
 
 
 const auth = getAuth(app)
@@ -19,8 +19,18 @@ const useFirebase = ()=>{
             console.log(user);
         })
     }
-    
-    return {user, signInWithGoogle}
+
+    const handleSignOut = ()=> {
+        signOut(auth)
+        .then(() => {})
+    }
+    useEffect ( ()=>{
+
+        onAuthStateChanged(auth, user => {
+            setUser(user);
+        })
+    }, [])
+    return {user, handleSignOut, signInWithGoogle}
 }
 
 export default useFirebase;
