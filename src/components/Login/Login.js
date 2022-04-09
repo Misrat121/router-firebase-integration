08734a8ca169dd002b2299af40d5c9a0a66f7263
Misrat121 <you@example.com>
2022-04-09 20:39:41 +0600

    sign in pop up added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {signInWithGoogle} = useFirebase();
     return (
         <div>
             <h3>Please Log in</h3>
+          <div style={{margin:'2 0px'}}>
+          <button onClick={signInWithGoogle}>Google Sign In</button>
+          </div>
+            <form>
+                <input type="email" placeholder="Your Email" />
+                <br />
+                <input type="password" name="" id="" placeholder='Password'/>
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
---
+++ src/components/Register/Register.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h1>Please Register Now!</h1>
+            <form action="">
+                <input type="text" placeholder="Name" />
+                <br />
+                <input type="email" placeholder="Your Email" />
+                <br />
+                <input type="password" name="" id="" placeholder='Password'/>
+                <br />
+                <input type="submit" value="Register" />
+            </form>
+        </div>
+    );
+};
+
+export default Register;
